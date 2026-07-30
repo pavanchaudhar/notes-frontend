481a732d5f5f9fa086a8a7ef0786f53819b0c625
pavanchaudhar <pavanchaudhari035@gmail.com>
2026-07-30 21:33:43 +0530

    solve login problum

--- frontend/frontendapp/static/js/dashboard.js
+++ frontend/frontendapp/static/js/dashboard.js
@@ -2,10 +2,40 @@ console.log("Dashboard Loaded");
 
 const token = localStorage.getItem("access");
 
-console.log("Access Token:");
-console.log(token);
-
 if (!token) {
     alert("Please login first.");
     window.location.href = "/";
+}
+
+loadNotes();
+
+async function loadNotes() {
+
+    const response = await fetch("/api/notes/", {
+        method: "GET",
+        headers: {
+            "Authorization": `Bearer ${token}`
+        }
+    });
+
+    const notes = await response.json();
+
+    console.log(notes);
+
+    const container = document.getElementById("notesContainer");
+
+    container.innerHTML = "";
+
+    notes.forEach(note => {
+
+        container.innerHTML += `
+            <div>
+                <h3>${note.title}</h3>
+                <p>${note.content}</p>
+                <hr>
+            </div>
+        `;
+
+    });
+
 }
--- frontend/frontendapp/static/js/login.js
+++ frontend/frontendapp/static/js/login.js
@@ -7,7 +7,7 @@ async function login() {
     const username = document.getElementById("username").value;
     const password = document.getElementById("password").value;
 
-    const response = await fetch("http://127.0.0.1:8001/api/token/", {
+    const response = await fetch("http://127.0.0.1:8000/api/token/", {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
@@ -21,9 +21,17 @@ async function login() {
     const data = await response.json();
 
     console.log(data);
+
+    if (response.ok) {
+    console.log("Login Successful");
+
+    localStorage.setItem("access", data.access);
+    localStorage.setItem("refresh", data.refresh);
+
+    alert("Redirecting to Dashboard");
+
+    window.location.href = "/dashboard/";
+    } else {
+        alert(data.detail);
+    }
 }
-
-localStorage.setItem("access", data.access);
-localStorage.setItem("refresh", data.refresh);
-
-window.location.href = "/dashboard/";
